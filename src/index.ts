76651693172ccbe192cf243e4a659ecace7f3431
Sergey Shandar <sergey.shandar@gmail.com>
2019-03-06 17:28:52 -0800

    print all tags

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,32 @@
+import * as path from "path"
+import * as fs from "@ts-common/fs"
+import * as md from "@ts-common/commonmark-to-markdown"
+import * as azureMd from "@azure/openapi-markdown"
+
 // Command-Line Interface. The function should return `Promise<number>`,
 // it shouldn't throw exceptions or reject the promise.
 export const cli = async () => {
-  console.log("Hello world!")
-  return 1
+  try {
+    await validateAll(path.resolve("./"))
+    return 0
+  } catch (e){
+    console.error(e)
+    return 1
+  }
+}
+
+const validateAll = async (dir: string): Promise<void> => {
+  for await (const f of fs.recursiveReaddir(dir)) {
+    if (path.basename(f).toLowerCase() === "readme.md") {
+      validateReadMe(f)
+    }
+  }
+}
+
+const validateReadMe = async (readMePath: string): Promise<void> => {
+  const m = md.parse((await fs.readFile(readMePath)).toString())
+  const tags = azureMd.getTagsToSettingsMapping(m.markDown)
+  for (const tag in tags) {
+    console.log(tag)
+  }
 }
