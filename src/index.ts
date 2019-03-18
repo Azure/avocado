716fa1d8b5033dee18097cfc4293a799652df8a5
Sergey Shandar <sergey.shandar@gmail.com>
2019-03-18 15:59:38 -0700

    resolve references

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,9 @@ import * as md from "@ts-common/commonmark-to-markdown"
 import * as azureMd from "@azure/openapi-markdown"
 import * as ai from "@ts-common/async-iterator"
 import * as jp from "@ts-common/json-parser"
+import * as it from "@ts-common/iterator"
+import * as json from "@ts-common/json"
+import * as sm from "@ts-common/string-map"
 
 export const cli = async <T>(f: (path: string) => AsyncIterable<T>): Promise<number> => {
   try {
@@ -40,37 +43,77 @@ export const avocado = (dir: string): ai.AsyncIterableEx<Error> =>
     .filter(f => path.basename(f).toLowerCase() === "readme.md")
     .flatMap(validateReadMe)
 
+type Ref = {
+  readonly url: string
+  readonly pointer: string
+}
+
+const parseRef = (ref: string): Ref => {
+  const i = ref.indexOf("#")
+  return i < 0 ? { url: "", pointer: ref } : { url: ref.substr(0, i), pointer: ref.substr(i + 1) }
+}
+
+const getRefs = (j: json.Json): it.IterableEx<string> =>
+  json.isObject(j) ?
+    sm.entries(j).flatMap(
+      ([k, v]) => k === "$ref" && typeof v === "string" ?
+        it.concat([v]) :
+        getRefs(v)
+    ) :
+  it.isArray(j) ? it.flatMap(j, getRefs) :
+  it.empty()
+
+const resolveReferences = async (readMePath: string, fileNames: Set<string>) => {
+  let fileNamesToCheck = it.toArray(fileNames)
+  const errors: Error[] = []
+  while (fileNamesToCheck.length !== 0) {
+    const newFileNames = []
+    for (const fileName of fileNamesToCheck) {
+      if (await fs.exists(fileName)) {
+        const file = await fs.readFile(fileName)
+        const document = jp.parse(
+          fileName,
+          file.toString(),
+          e => errors.push({ code: "JSON_PARSE", error: e})
+        )
+        const dir = path.dirname(fileName)
+        const refFiles = getRefs(document)
+          .map(v => parseRef(v).url)
+          .filter(u => u !== "")
+          .map(u => path.resolve(path.join(dir, u)))
+        for (const rf of refFiles) {
+          if (!fileNames.has(rf)) {
+            fileNames.add(rf)
+            newFileNames.push(rf)
+          }
+        }
+      } else {
+        errors.push({
+          code: "NO_OPEN_API_FILE_FOUND",
+          message: "the OpenAPI file is not found but it is referenced from the readme file.",
+          readMeUrl: readMePath,
+          openApiUrl: fileName
+        })
+      }
+    }
+    fileNamesToCheck = newFileNames
+  }
+  return errors
+}
+
 const validateReadMe = (readMePath: string): ai.AsyncIterableEx<Error> =>
   ai.iterable<Error>(async function *() {
     const file = await fs.readFile(readMePath)
     const m = md.parse(file.toString())
     const dir = path.dirname(readMePath)
     const inputFiles = azureMd.getInputFiles(m.markDown).toArray()
-    for (const p of inputFiles) {
-      const fullPath = path.join(dir, p)
-      if (!await fs.exists(fullPath)) {
-        yield {
-          code: "NO_OPEN_API_FILE_FOUND",
-          message: "the OpenAPI file is not found but it is referenced from the readme file.",
-          readMeUrl: readMePath,
-          openApiUrl: fullPath
-        }
-      }
-    }
     const normInputFiles = inputFiles.map(f => path.resolve(path.join(dir, f)))
     const set = normInputFiles.reduce((set, v) => set.add(v), new Set<string>())
-    const files = fs.recursiveReaddir(dir)
-    for await (const f of files) {
+    const fileNames = fs.recursiveReaddir(dir)
+    const errors = await resolveReferences(readMePath, set)
+    yield *errors
+    for await (const f of fileNames) {
       if (path.extname(f) === ".json") {
-        const file = await fs.readFile(f)
-        const errors: jp.ParseError[] = []
-        jp.parse(f, file.toString(), e => errors.push(e))
-        for (const e of errors) {
-          yield {
-            code: "JSON_PARSE",
-            error: e
-          }
-        }
         if (!set.has(f)) {
           yield {
             code: "UNREFERENCED_OPEN_API_FILE",
