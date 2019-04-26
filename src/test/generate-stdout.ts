03dbcfedb4d00df0fda30016ea0eaf6730ff52a3
Sergey Shandar <sergey.shandar@gmail.com>
2019-04-26 11:24:59 -0700

    simplified

--- src/test/generate-stdout.ts
+++ src/test/generate-stdout.ts
@@ -11,9 +11,5 @@ export const generate = () => iterator.repeat('Hello world!', 100000)
 /**
  * The function prints to `stdout` a lot of `Hello world!` strings to test `exec` function.
  */
-export const print = () => {
-  for (const v of generate()) {
-    // tslint:disable-next-line:no-console
-    console.log(v)
-  }
-}
+// tslint:disable-next-line:no-console
+export const print = () => generate().forEach(v => console.log(v))
