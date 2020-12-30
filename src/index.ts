e5069dc97cfc74059e27a6c35c69fdbedb2ea053
Ruoxuan Wang <52271048+ruowan@users.noreply.github.com>
2020-12-30 14:08:28 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -587,7 +587,7 @@ const avocadoForDevOps = (pr: devOps.PullRequestProperties): asyncIt.AsyncIterab
       const fileChanges = await pr.diff()
 
       // remove existing errors.
-     avocado will directly report it even though it's not a new involved error in the pull request.*/
+      /* avocado will directly report it even though it's not a new involved error in the pull request.*/
       for (const e of targetMap.keys()) {
         const error = sourceMap.get(e)
         if (error !== undefined && !devOps.isPRRelatedError(fileChanges, error)) {
