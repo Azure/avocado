f2b48366ecb0266ecb40b462e0798d6fb252a1b8
Tianxiang Chen <tianxchen@microsoft.com>
2022-11-10 15:51:01 +0800

    add inlcude arg

--- bin/cli.js
+++ bin/cli.js
@@ -10,11 +10,20 @@ var argv = require('yargs')
   .describe('d', 'run avocado under directory')
   .option('excludePaths', {
     type: 'array',
-    desc: 'array contains path patterns to be ignored'
+    desc: 'array contains path patterns to be ignored',
+  })
+  .option('includePaths', {
+    type: 'array',
+    desc:
+      'array contains path patterns to be included. If this option is not set, all files will be included. If this option is set, only files that match at least one pattern will be included',
   })
   .help('h')
   .alias('h', 'help').argv
 
 
 
-cli.run(index.avocado, index.UnifiedPipelineReport(argv.f), {cwd: process.cwd(), env: process.env, args: {dir: argv.d, excludePaths: argv.excludePaths}})
+cli.run(index.avocado, index.UnifiedPipelineReport(argv.f), {
+  cwd: process.cwd(),
+  env: process.env,
+  args: { dir: argv.d, excludePaths: argv.excludePaths, includePaths: argv.includePaths },
+})
