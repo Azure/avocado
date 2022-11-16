191cd1999500151a0ce6444d1e49ceac0da8d2b4
ruowan <ruowan@microsoft.com>
2022-11-16 05:57:12 +0000

    finalize interface

--- src/docs.ts
+++ src/docs.ts
@@ -4,24 +4,17 @@
 // https://opensource.org/licenses/MIT
 
 export interface IService {
-  toc_title: string
-  url_group: string
   readme_files?: string[]
   swagger_files?: ISwaggerFile[]
-  excluded_files?: string[]
 }
 
 export interface ISwaggerFile {
   source: string
 }
 
-// How to mapping returned swagger files with correct sub_group_toc_title.
-
-export type SwaggerFiles = {
-  version: VersionVariable // version of the swagger files or version of tag
-  swaggerFiles: ISwaggerFile[]
+export type SwaggerFile = {
+  source: string
+  swagger_file: ISwaggerFile
 }
 
-export type VersionVariable = 'version' | 'preview_version' | 'stable_version'
-
-export type GetSwaggerFiles = (specRootFilePath: string, service: IService) => ISwaggerFile[]
+export type GetSwaggerFiles = (rootPath: string, service: IService) => SwaggerFile[]
