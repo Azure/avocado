1d52b056b32d5ee8172b914c9cca9a47b472a778
Mike Harder <mharder@microsoft.com>
2025-10-06 18:35:34 +0000

    add comments for disabled rules

--- eslint.config.js
+++ eslint.config.js
@@ -6,5 +6,10 @@ import tseslint from 'typescript-eslint'
 /** @type {import('eslint').Linter.Config[]} */
 export default defineConfig({ ignores: ['dist/**'] }, eslint.configs.recommended, tseslint.configs.recommended, {
   languageOptions: { globals: globals.node },
-  rules: { '@typescript-eslint/no-explicit-any': 'off', '@typescript-eslint/no-require-imports': 'off' },
+  rules: {
+    // 17 errors
+    '@typescript-eslint/no-explicit-any': 'off',
+    // 5 errors
+    '@typescript-eslint/no-require-imports': 'off',
+  },
 })
