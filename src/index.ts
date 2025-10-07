83c7afed03693aeea2dda95e869090c3ae9ec1f6
Mike Harder <mharder@microsoft.com>
2025-10-07 18:19:28 +0000

    Sort date-based versions first

--- src/index.ts
+++ src/index.ts
@@ -469,7 +469,7 @@ export const validateRPMustContainAllLatestApiVersionSwagger = (dir: string): it
 export const mergePathTable = (pathTable: PathTable, newPathTable: PathTable): PathTable => {
   for (const [key, value] of newPathTable) {
     if (pathTable.has(key)) {
-      if (pathTable.get(key)!.apiVersion < value.apiVersion) {
+      if (compareApiVersion(pathTable.get(key)!.apiVersion, value.apiVersion) < 0) {
         pathTable.set(key, value)
       }
     } else {
@@ -477,6 +477,32 @@ export const mergePathTable = (pathTable: PathTable, newPathTable: PathTable): P
     }
   }
   return pathTable
+}
+
+// apiVersion may be date-based ("2025-07-01") or non-date-based ("7.0", "v7.0", etc).  All recent versions should be
+// date-based, so sort all date-based versions later than all non-date-based versions.
+const compareApiVersion = (a: string, b: string): number => {
+  const dateA = containsDate(a)
+  const dateB = containsDate(b)
+  if (dateA && !dateB) {
+    return 1
+  } else if (!dateA && dateB) {
+    return -1
+  } else {
+    // Both "a" and "b" are either date-based or non-date-based, so perform an invariant string comparison
+    return invariantCompare(a, b)
+  }
+}
+
+// Returns true if a string contains a substring that looks like a date "YYYY-MM-DD"
+const containsDate = (s: string): boolean => {
+  const dateRegex = /\d{4}-\d{2}-\d{2}/
+  return dateRegex.test(s)
+}
+
+const invariantCompare = (a: string, b: string): number => {
+  // JS doesn't provide a built-in invariantCompare(), and recommends this
+  return a.localeCompare(b, /* locales */ 'und', { sensitivity: 'variant' })
 }
 
 export const diffPathTable = (defaultPathTable: PathTable, latestPathTable: PathTable): any[] => {
