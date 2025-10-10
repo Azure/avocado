69188636957cf379b32b577ae1655c64b5fddb0f
Mike Harder <mharder@microsoft.com>
2025-10-10 02:35:23 +0000

    increase test timeout to 100s

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,5 +5,7 @@ export default defineConfig({
     // Our test files currently use pattern "foo-test.js" instead of the default "foo.test.js",
     // so we replace the first "." in the default pattern with "-"
     include: ['**/*-{test,spec}.?(c|m)[jt]s?(x)'],
+    // git-test.ts can be slow on Windows
+    testTimeout: 100000,
   },
 })
