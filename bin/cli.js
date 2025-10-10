1f8edd5eb6556c15fb7fd5e16440f906b0a08ebc
Mike Harder <mharder@microsoft.com>
2025-10-10 20:20:31 +0000

    convert require to import

--- bin/cli.js
+++ bin/cli.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
-const cli = require('../dist/cli')
-const index = require('../dist/index')
 
-var argv = require('yargs')
+import yargs from 'yargs'
+import { run } from '../dist/cli.js'
+import { avocado, UnifiedPipelineReport } from '../dist/index.js'
+
+var argv = yargs
   .usage('Usage: avocado [options]')
   .alias('f', 'file')
   .describe('f', 'output detail result to log file')
@@ -14,15 +16,12 @@ var argv = require('yargs')
   })
   .option('includePaths', {
     type: 'array',
-    desc:
-      'array contains path patterns to be included. If this option is not set, all files will be included. If this option is set, only files that match at least one pattern will be included',
+    desc: 'array contains path patterns to be included. If this option is not set, all files will be included. If this option is set, only files that match at least one pattern will be included',
   })
   .help('h')
   .alias('h', 'help').argv
 
-
-
-cli.run(index.avocado, index.UnifiedPipelineReport(argv.f), {
+run(avocado, UnifiedPipelineReport(argv.f), {
   cwd: process.cwd(),
   env: process.env,
   args: { dir: argv.d, excludePaths: argv.excludePaths, includePaths: argv.includePaths },
