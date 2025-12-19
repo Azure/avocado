d6871c242c978029b3229a718cf44097aa487781
Mike Harder <mharder@microsoft.com>
2025-12-19 00:36:42 +0000

    [vitest.config.ts] Only include dir "src/test"

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
   test: {
+    dir: './src/test',
+
     // Our test files currently use pattern "foo-test.js" instead of the default "foo.test.js",
     // so we replace the first "." in the default pattern with "-"
     include: ['**/*-{test,spec}.?(c|m)[jt]s?(x)'],
